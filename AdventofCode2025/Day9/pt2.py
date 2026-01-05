386b47a5731f94b852b638f4ff191a5b3261cf2c
Quad14 <jgehring456@gmail.com>
2026-01-04 18:48:00 -0600

    Completed Day 9

--- AdventofCode2025/Day9/pt2.py
+++ AdventofCode2025/Day9/pt2.py
@@ -1,86 +1,70 @@
 import math
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
-connectionsMade = 0
-numberOfConnectionstoMake = 1000
-numberOfCircuitsMutiplied = 3
-boxes = []
-circuits = []
-remainingBoxes = []
-edges = []
+points = []
+largestArea = 0
+largestPointA = None
+largestPointB = None
+largestPointC = None
+largestPointD = None
+xRanges = []
+yRanges = []
 
-class Box:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.name = f"({x}, {y}, {z})"
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
 
 file_object = open("input.txt", "r")
+# First path populates points
 for line in file_object:
     line = line.strip()
-    x, y, z = map(int, line.split(","))
-    newBox = Box(x, y, z)
-    newCircuit = [newBox]
-    circuits.append(newCircuit)
-    boxes.append(newBox)
+    x, y = map(int, line.split(","))
+    newPoint = Point(x, y)
+    points.append(newPoint)
+
+polygon = Polygon(points)
+
+for point in points:
+    print(f"Point: ({point.x}, {point.y})")
+
+def calculateSizeOfRectangle(p1, p2):
+    length = abs(p1.x - p2.x) + 1
+    width = abs(p1.y - p2.y) + 1
+    return length * width
+
+for pointA in points:
+    for pointB in points:
+        if pointA != pointB:
+            area = calculateSizeOfRectangle(pointA, pointB)
+            if area > largestArea:
+                pointC = Point(pointA.x, pointB.y)
+                pointD = Point(pointB.x, pointA.y)
+                testPolygon = Polygon([pointA, pointC, pointB, pointD])
+                # print(f"PointC: ({pointC.x}, {pointC.y}), PointD: ({pointD.x}, {pointD.y})")
+                # cFound = False
+                # dFound = False
+                # if ((pointC in points) or polygon.contains(pointC)):
+                #     cFound = True
+                # if ((pointD in points) or polygon.contains(pointD)):
+                #     dFound = True
+                if polygon.covers(testPolygon):
+                    print(f"New Largest Area Found: {area}")
+                    largestArea = area
+                    largestPointA = pointA
+                    largestPointB = pointB
+                    largestPointC = pointC
+                    largestPointD = pointD
 
 
-def distance(x1, y1, z1, x2, y2, z2):
-    return ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)  # squared distance for efficiency
 
-def updateClosestBoxes():
-    global boxes
-    for j in range(0, len(boxes)-1):
-        box = boxes[j]
-        print(f"Finding edges for Box({box.x}, {box.y}, {box.z})")
-        for k in range(j + 1, len(boxes)):
-            secondBox = boxes[k]
-            if secondBox != box:
-                newdistance = distance(box.x, box.y, box.z, secondBox.x, secondBox.y, secondBox.z)
-                edge = (box, secondBox, newdistance)
-                edges.append(edge)
-                    # print(f"Edge added between Box({box.x}, {box.y}, {box.z}) and Box({secondBox.x}, {secondBox.y}, {secondBox.z}) at distance {newdistance}")
-
-    edges.sort(key=lambda edge: edge[2])
+print(largestArea)
+print(f"Largest Points: A({largestPointA.x}, {largestPointA.y}), B({largestPointB.x}, {largestPointB.y})")
+print(f"                  C({largestPointC.x}, {largestPointC.y}), D({largestPointD.x}, {largestPointD.y})")
 
 
-def makeConnection(boxA, boxB):
-    global connectionsMade
-    for circuitA in circuits:
-        if boxA in circuitA:
-            for circuitB in circuits:
-                if boxB in circuitB:
-                    # merge circuits
-                    if circuitA != circuitB:
-                        circuitA.extend(circuitB)
-                        circuits.remove(circuitB)
-                    connectionsMade += 1
-                    return True
-                            
-updateClosestBoxes()
-
-# for edge in edges:
-#     print(f"Edge between Box({edge[0].x}, {edge[0].y}, {edge[0].z}) and Box({edge[1].x}, {edge[1].y}, {edge[1].z}) at distance {edge[2]}")
-
-index = 0
-lastEdge = None
-for edge in edges:
-    if len(circuits) == 1:
-        break
-    lastEdge = edge
-    makeConnection(edge[0], edge[1])
-
-circuits = sorted(circuits, key=lambda circuit: len(circuit), reverse=True)
-# print("MAKE CONNECTION")
-# for circuit in circuits:
-#     if len(circuit) > 1:
-#         print("Circuit:")
-#         for box in circuit:
-#             print(f"  Box({box.x}, {box.y}, {box.z})")
-# print(f"Connections made: {connectionsMade}")
-firstCircuit = lastEdge[0]
-lastCircuit = lastEdge[1]
-print(f"First box in Circuit 1 is Box({firstCircuit.x}, {firstCircuit.y}, {firstCircuit.z})")
-print(f"Last box in Circuit 1 is Box({lastCircuit.x}, {lastCircuit.y}, {lastCircuit.z})")
-total = firstCircuit.x * lastCircuit.x
-print(total)
+# row ranges between points y dims
+# We get p1 and p2 from the for loops above but need to also determine p3 and p4
+# and make sure that p3 and p4 are in the ranges of other points to form a valid rectangle
+# p3.x = p1.x, p3.y = p2.y
